d9995fc4024dde2c726aae0c0f35ed76f611ae45
Michael Boroski <mjboroski@gmail.com>
2018-07-09 22:19:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-function produceDrivingRange(){
-  return function(){
+function produceDrivingRange(blockRange){
+  return function(startingBlock, endingBlock){
 
+    let start = parseInt(startingBlock);
+    let end = parseInt(endingBlock);
+    let distanceToTravel = Math.abs(end - start);
+    let difference = blockRange - distanceToTravel;
+
+    if( difference > 0){
+      return `within range by ${difference}`
+    } else {
+      return `${Math.abs(difference)} blocks out of range`
+    }
   }
+
 }
 
-function produceTipCalculator(){
-  return function(){
-
+function produceTipCalculator(percentage){
+  return function(rideFare){
+    return rideFare*percentage;
   }
 }
 
 function createDriver (){
   let driverId=0
   return class Driver{
-    constructor()
+    constructor(name){
+      this.id = ++driverId
+      this.name = name
+    }
   }
-
 }
