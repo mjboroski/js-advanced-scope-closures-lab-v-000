a85ab68977f408d7ae66e2c3604b068fe165e412
Michael Boroski <mjboroski@gmail.com>
2018-07-09 22:16:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function produceDrivingRange(){
+  return function(){
+
+  }
+}
+
+function produceTipCalculator(){
+  return function(){
+
+  }
+}
+
+function createDriver (){
+  let driverId=0
+  return class Driver{
+    constructor()
+  }
+
+}
